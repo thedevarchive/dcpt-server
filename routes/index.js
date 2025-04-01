647b9a4ed384e9464725fbca3d5ef04b01ff50cf
Alyssa Ngo <amaengo@protonmail.com>
2025-04-01 20:43:43 +1000

    Added scoring for new IT career

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,6 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET all personality test results */
-router.get("/results", (req, res) => {
-  const results = [
-    { title: "Front-End Developer", description: "Focuses on the user interface and user experience of web applications. They work with HTML, CSS, JavaScript, and frameworks like React or Angular." },
-    { title: "Back-End Developer", description: "Works on the server side, handling the logic, database interactions, and server configuration. They often use languages like Python, Java, Ruby, or Node.js." },
-    { title: "Full-Stack Developer", description: "Handles both front-end and back-end development. They have a broad skill set that allows them to build complete applications from start to finish." },
-    { title: "DevOps Engineer", description: "Focuses on the integration of development and operations. They work on automating deployment, monitoring systems, and managing infrastructure using tools like Docker, Kubernetes, and CI/CD pipelines." },
-    { title: "Database Administrator", description: "Specializes in managing and optimizing databases. They ensure data integrity, performance, and security, and work with databases like MySQL, PostgreSQL, or MongoDB." },
-    { title: "Game Developer", description: "Game developers are responsible for creating and producing video games for various platforms, including consoles, PCs, and mobile devices. Their work involves designing, programming, testing, and refining games to provide an engaging user experience." },
-  ];
-
-  res.json(results);
-});
-
 /* GET all questions */
 router.get("/api/question", async (req, res) => {
   const questions = await req.db.from("questions").select("question_text");
@@ -44,7 +30,7 @@ router.post("/result/calculate", async (req, res) => {
   const question_count = await getQuestionCount(req);
   console.log(question_count);
 
-  const scores = { FED: 0, BED: 0, FSD: 0, DOE: 0, DBA: 0, GDV: 0, QAT: 0, MAD: 0, CSX: 0 };
+  const scores = { FED: 0, BED: 0, FSD: 0, DOE: 0, DBA: 0, GDV: 0, QAT: 0, MAD: 0, CSX: 0, BCD: 0 };
 
   /*
 
@@ -59,6 +45,7 @@ router.post("/result/calculate", async (req, res) => {
   QAT = QA Tester / Quality Assurance Tester
   MAD = Mobile App Developer
   CSX = Cyber Security Expert
+  BCD = Blockchain Developer
 
   TODO: 
   Update README 
